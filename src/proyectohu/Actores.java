33f1c97f5597452bbf40fa5c9e4035519553d8fe
DanielaOdileth <odilethpadilla@unitec.edu>
2015-09-19 19:26:47 -0600

    Ventana

---
+++ src/proyectohu/Actores.java
@@ -0,0 +1,52 @@
+package proyectohu;
+
+import java.util.ArrayList;
+
+public class Actores {
+    String nombre;
+    String edad;
+    String nacionalidad;
+    ArrayList<Peliculas> lista_peliculas;
+
+    public Actores() {
+    }
+
+    public Actores(String nombre, String edad, String nacionalidad, ArrayList<Peliculas> lista_peliculas) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.nacionalidad = nacionalidad;
+        this.lista_peliculas = lista_peliculas;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public ArrayList<Peliculas> getLista_peliculas() {
+        return lista_peliculas;
+    }
+
+    public void setLista_peliculas(ArrayList<Peliculas> lista_peliculas) {
+        this.lista_peliculas = lista_peliculas;
+    } 
+}
